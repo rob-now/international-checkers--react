cb3bb4f56705525086bc5537e436456193747a25
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-13 12:54:18 +0200

    Added CheckersView component

---
+++ src/CheckersView.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class CheckersView extends Component {
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CheckersView
