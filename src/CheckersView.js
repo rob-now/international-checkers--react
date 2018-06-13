6e19c53829be0c5bedc6c45f7441569750a825b7
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-13 15:11:36 +0200

    Added initial view of the board and initial state of the board

--- src/CheckersView.js
+++ src/CheckersView.js
@@ -1,10 +1,122 @@
 import React, {Component} from 'react'
+import './CheckersView.css'
 
 class CheckersView extends Component {
-  render(){
-    return(
-      <div>
-        
+
+  state = {
+    boardState: [
+      [0, 'b', 0, 'b', 0, 'b', 0, 'b', 0, 'b'],
+      ['b', 0, 'b', 0, 'b', 0, 'b', 0, 'b', 0],
+      [0, 'b', 0, 'b', 0, 'b', 0, 'b', 0, 'b'],
+      ['b', 0, 'b', 0, 'b', 0, 'b', 0, 'b', 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 'w', 0, 'w', 0, 'w', 0, 'w', 0, 'w'],
+      ['w', 0, 'w', 0, 'w', 0, 'w', 0, 'w', 0],
+      [0, 'w', 0, 'w', 0, 'w', 0, 'w', 0, 'w'],
+      ['w', 0, 'w', 0, 'w', 0, 'w', 0, 'w', 0]
+    ]
+  }
+
+  render() {
+    return (
+      <div className="Checkers__Board">
+        <div className="Checkers__Row">
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+        </div>
+        <div className="Checkers__Row">
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+        </div>
+        <div className="Checkers__Row">
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+          <div className="Checkers__Square">
+
+          </div>
+        </div>
       </div>
     )
   }
