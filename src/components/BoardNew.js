11db95567adecdcb25fe8dcf954e7a62860dc2b8
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-18 17:37:11 +0200

    Displaying numbers and letters on boards edges

--- src/components/BoardNew.js
+++ src/components/BoardNew.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react'
+import './BoardNew.css'
 
 const boardSize = 12
 
 class BoardNew extends Component {
 
-  renderRow = i => {
+  renderRow = (x) => {
     //Storing cells in array
     let cells = []
     for (let j = 0; j < boardSize; j += 1) {
-      cells[j] = this.renderCell((i * boardSize) + j)
+      cells[j] = this.renderCell((x * boardSize) + j, x, j)
     }
     console.log('cells', cells)
     return (
@@ -20,12 +21,24 @@ class BoardNew extends Component {
     )
   }
 
-  renderCell = i => {
-    console.log('rendering cell', i)
+  renderCell = (cellId, x, y) => {
+    console.log('rendering cell', cellId, x, y)
+    const letters = [null, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', null]
     return <div
       className="board--cell"
     >
-      cell
+      {
+        (y === 0 && x > 0 && x < 11) || (y === 11 && x > 0 && x < 11) ?
+          boardSize - x - 1 :
+          (x === 0 && y > 0 && y < 11) || (x === 11 && y > 0 && y < 11) ?
+            letters.filter(
+              (_, index) =>
+                index === y
+            ) :
+            (y === 0 && x === 0) || (y === 0 && x === 11) || (y === 11 && x === 0) || (y === 11 && x === 11) ?
+              '' :
+              'inner'
+      }
     </div>
   }
 
